c024d9e6b84b11abe32c974d7af29d5722397e94
marshall62 <marshall62@gmail.com>
2019-05-06 16:33:27 -0400

    stopped using pytest-datafiles

--- tests/test_partner.py
+++ tests/test_partner.py
@@ -2,26 +2,20 @@ import partner
 import os
 import pytest
 
-FIXTURE_DIR = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    'files',
-)
 
-@pytest.mark.datafiles(os.path.join(FIXTURE_DIR, 'notexist.csv'))
-def test_config0 (datafiles):
-    fname = datafiles[0]
+def test_config0 ():
+    fname = os.path.dirname(os.path.realpath(__file__)) + '/files/notexist.csv'
     with pytest.raises(Exception):
         pairs = partner.readConfig(fname)
 
-@pytest.mark.datafiles(os.path.join(FIXTURE_DIR, 'empty_config.csv'))
-def test_config1 (datafiles):
-    fname = datafiles[0]
+def test_config1 ():
+    fname = os.path.dirname(os.path.realpath(__file__)) + '/files/empty_config.csv'
     pairs = partner.readConfig(fname)
     assert [] == pairs
 
-@pytest.mark.datafiles(os.path.join(FIXTURE_DIR, 'config.csv'))
-def test_config2 (datafiles):
-    fname = datafiles[0]
+
+def test_config2 ():
+    fname = os.path.dirname(os.path.realpath(__file__)) + '/files/config.csv'
     pairs = partner.readConfig(fname)
     assert "David Marshall" == pairs[0][0]
     assert "Paula Nieman" == pairs[0][1]
