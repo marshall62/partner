389f2cf2a2255dad8e888ddff52b7e2e0181d6e8
marshall62 <marshall62@gmail.com>
2020-03-03 17:01:31 -0500

    added files to gitignore deleted others.

--- partner/models.py
+++ partner/models.py
@@ -141,26 +141,6 @@ class AttendanceEntry(db.Model):
         return '<Entry {} {}>'.format(self.date, self.value)
 
 
-class User(db.Model):
-    __tablename__ = 'user'
-    email = db.Column(db.String, primary_key=True)
-    password = db.Column(db.LargeBinary)
-    authenticated = db.Column(db.Boolean, default=False)
-
-    def is_active(self):
-        return True
-
-    def get_email(self):
-        return self.email
-
-    def is_authenticated(self):
-        return self.authenticated
-
-    def is_anonymous(self):
-        return False
-
-    def to_dict (self):
-        return { 'email': self.email, 'authenticated': self.authenticated}
 
 
 class Instructor(db.Model):
