3c2b53f03f36c3948efbefdb2e85c769f4112a43
marshall62 <marshall62@gmail.com>
2020-06-27 09:38:35 -0400

    added types to util functions about dates.

--- partner/util.py
+++ partner/util.py
@@ -1,7 +1,8 @@
 import time
-import datetime
+from typing import Dict
+from datetime import date
 
-def get_cur_date (sep="/", yy_format=True):
+def get_cur_date (sep="/", yy_format=True) -> str:
     mm = time.strftime("%m")
     dd = time.strftime("%d")
     if yy_format:
@@ -10,45 +11,45 @@ def get_cur_date (sep="/", yy_format=True):
         y = time.strftime("%Y")  #yyyy
     return sep.join([mm, dd, y])
 
-def parse_date (mmddyyyy):
+def parse_date (mmddyyyy: str) -> date:
     parts = mmddyyyy.split('/')
     y = int(parts[2])
     m = int(parts[0])
     d = int(parts[1])
-    return datetime.date(year=y, month=m, day=d)
+    return date(year=y, month=m, day=d)
 
-def is_in_past (dt):
-    today = datetime.date.today()
+def is_in_past (dt: date):
+    today = date.today()
     return today.toordinal() > dt.toordinal()
 
-def is_today (dt):
-    today = datetime.date.today()
+def is_today (dt: date):
+    today = date.today()
     return today.toordinal() == dt.toordinal()
 
-def today ():
-    return datetime.date.today()
+def today () -> date:
+    return date.today()
 
 def get_current_year ():
     return today().year
 
-def get_term (dt):
+def get_term (dt: date) -> str:
     if dt.month < 6:
         return 'spring'
     else:
         return 'fall'
 
-def get_current_term ():
-    t = today() #type: datetime.date
+def get_current_term () -> str:
+    t = today()
     return get_term(t)
 
-def mdy_to_date (mdy):
+def mdy_to_date (mdy: str) -> date:
     m,d,y = mdy.split('/')
-    return datetime.date(month=int(m), day=int(d), year=int(y))
+    return date(month=int(m), day=int(d), year=int(y))
 
-def date_to_mdy (date):
-    return date.strftime("%m/%d/%Y")
+def date_to_mdy (d: date) -> str:
+    return d.strftime("%m/%d/%Y")
 
-def str_to_dict (str):
+def str_to_dict (s: str) -> Dict[str, str]:
     d = {}
     for pair in str.split(','):
         k,v = pair.split(':')
@@ -56,12 +57,12 @@ def str_to_dict (str):
     return d
 
 # convert yyyy-mm-ddTHH:mm:ss to datetime.date
-def jsdate_to_date (str):
-    y,m,d = str.split('T')[0].split('-')
-    return datetime.date(month=int(m), day=int(d), year=int(y))
+def jsdate_to_date (s: str) -> date:
+    y,m,d = s.split('T')[0].split('-')
+    return date(month=int(m), day=int(d), year=int(y))
 
-def str_to_date (str):
-    if 'T' in str:
-        return jsdate_to_date(str)
+def str_to_date (s: str) -> date:
+    if 'T' in s:
+        return jsdate_to_date(s)
     else:
-        return mdy_to_date(str)
+        return mdy_to_date(s)
